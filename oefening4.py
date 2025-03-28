c68b18d01b1afef5c372217ac4d945de0f0c2fc5
ilian <iliancleynhens@gmail.com>
2025-03-28 13:56:27 +0100

    klaar

--- oefening4.py
+++ oefening4.py
@@ -1,17 +1,15 @@
-def countTargetPairs(nums, target):
-    nums.sort()
-    count = 0
-    lengte = len(nums)
-    
-    for i in range(lengte - 1):
-        for j in range(i + 1, lengte):
-            if nums[i] + nums[j] < target:
-                count += 1
-            else:
-                count = count
+def Count
 
-    
-    return count
 
-print(countTargetPairs([-1, 1, 2, 3, 1], 2))
-print(countTargetPairs([-6, 2, 5, -2, -7, -1, 3], -2))
+
+
+
+
+
+
+
+
+
+
+
+
